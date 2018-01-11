7da4589eb9e6b71bc00c7e7b81f2f884b3599ffa
Trey Hoover <treyhoover@gmail.com>
2018-01-11 01:41:31 -0500

    Update Contact example

--- examples/contact-phone/src/components/Contact.js
+++ examples/contact-phone/src/components/Contact.js
@@ -17,9 +17,9 @@ const Contact = ({ children, img, name, company, phone, ...props }) => (
     {...props}
   >
     <Img w2 h2 w3-ns h3-ns br-100 src={img} alt={name} />
-    <Div className="pl3 flex-auto">
-      <Span className="f6 db black-70">{name}</Span>
-      <Span className="f6 db black-70">{company}</Span>
+    <Div pl3 flex-auto>
+      <Span f6 db black-70>{name}</Span>
+      <Span f6 db black-70>{company}</Span>
     </Div>
     <A href={`tel:${phone}`} f6 link blue hover-dark-gray>{phone}</A>
   </Li>
