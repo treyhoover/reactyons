5be75da82e1b39d8f5cfe561be8ec134dad5f9a9
Trey Hoover <treyhoover@gmail.com>
2018-02-13 22:52:27 -0500

    Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,71 +1,45 @@
 import typescript from 'typescript';
 import typescriptPlugin from 'rollup-plugin-typescript';
 import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel'
 import replace from 'rollup-plugin-replace';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import localResolve from "rollup-plugin-local-resolve";
 import pkg from './package.json';
-// import uglify from 'rollup-plugin-uglify';
+import uglify from 'rollup-plugin-uglify';
 
 const dev = 'development';
 const prod = 'production';
 
-const nodeEnv = parseNodeEnv(process.env.NODE_ENV);
+const env = parseNodeEnv(process.env.NODE_ENV);
 
-const plugins = [
-    peerDepsExternal(),
-    replace({
-        // The react sources include a reference to process.env.NODE_ENV so we need to replace it here with the actual value
-        'process.env.NODE_ENV': JSON.stringify(nodeEnv),
-    }),
-    // nodeResolve makes rollup look for dependencies in the node_modules directory
-    localResolve(),
-    nodeResolve({
-        browser: true,
-        preferBuiltins: false,
-        jsnext: true,
-        extensions: ['.js', '.ts', '.tsx'],
-    }),
-    commonjs({
-        // All of our own sources will be ES6 modules, so only node_modules need to be resolved with cjs
-        include: 'node_modules/**',
-        namedExports: {
-            // The commonjs plugin can't figure out the exports of some modules, so if rollup gives warnings like:
-            // ⚠️   'render' is not exported by 'node_modules/react-dom/index.js'
-            // Just add the mentioned file / export here
-            'node_modules/react-dom/index.js': [
-                'render',
-            ],
-            'node_modules/react/index.js': [
-                'Component',
-                'PropTypes',
-                'createElement',
-            ],
-        },
-    }),
-    typescriptPlugin({
-        // The current rollup-plugin-typescript includes an old version of typescript, so we import and pass our own version
-        typescript,
-        // rollup-plugin-typescript will inject some typescript helpers to your files (normally tsc will
-        // do this). They however have some ES6 keywords like const so they break older browsers.
-        // This instructs rollup-plugin-typescript to import tslib instead, which includes the same helpers
-        // in proper format.
-        importHelpers: true,
-    }),
-];
-
-//
-// if (nodeEnv === prod) {
-//     plugins.push(uglify());
-// }
-
-const sourcemap = nodeEnv === dev ? 'inline' : false;
-
-export default {
+const config = {
     name: "Tachyons",
-    plugins,
-    sourcemap,
+    plugins: [
+        typescriptPlugin({
+            // The current rollup-plugin-typescript includes an old version of typescript, so we import and pass our own version
+            typescript,
+            // rollup-plugin-typescript will inject some typescript helpers to your files (normally tsc will
+            // do this). They however have some ES6 keywords like const so they break older browsers.
+            // This instructs rollup-plugin-typescript to import tslib instead, which includes the same helpers
+            // in proper format.
+            importHelpers: true,
+        }),
+        peerDepsExternal(),
+        replace({
+            // The react sources include a reference to process.env.NODE_ENV so we need to replace it here with the actual value
+            'process.env.NODE_ENV': JSON.stringify(env),
+        }),
+        // nodeResolve makes rollup look for dependencies in the node_modules directory
+        localResolve(),
+        nodeResolve({
+            browser: true,
+            preferBuiltins: false,
+            jsnext: true,
+            extensions: ['.js', '.ts', '.tsx'],
+        }),
+    ],
+    sourcemap: env === dev ? 'inline' : false,
     input: './src/index.tsx',
     external: Object.keys(pkg.dependencies),
     output: {
@@ -74,9 +48,51 @@ export default {
     },
 };
 
+if (env === 'es' || env === 'cjs') {
+    config.output = { format: env };
+    config.external = ['symbol-observable'];
+    config.plugins.push(
+        babel({
+            plugins: ['external-helpers'],
+        })
+    )
+}
+
+if (env === 'development' || env === 'production') {
+    config.output = { format: 'umd' };
+    config.name = 'Redux';
+    config.plugins.push(
+        nodeResolve({
+            jsnext: true
+        }),
+        babel({
+            exclude: 'node_modules/**',
+            plugins: ['external-helpers'],
+        }),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify(env)
+        })
+    )
+}
+
+if (env === 'production') {
+    config.plugins.push(
+        uglify({
+            compress: {
+                pure_getters: true,
+                unsafe: true,
+                unsafe_comps: true,
+                warnings: false
+            }
+        })
+    )
+}
+
 function parseNodeEnv(nodeEnv) {
     if (nodeEnv === prod || nodeEnv === dev) {
         return nodeEnv;
     }
     return dev;
 }
+
+export default config;
