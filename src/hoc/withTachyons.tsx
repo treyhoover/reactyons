43db126795ed30481d6f9cea3e3acbab86cbe0be
Trey Hoover <treyhoover@gmail.com>
2018-02-13 20:44:09 -0500

    Refactor

---
+++ src/hoc/withTachyons.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import tachyons from "../classNames";
+
+const isValidClassName = str => typeof str === "string" && str.length > 0;
+
+export default (WrappedComponent) => class ComponentWithEventProps extends React.Component<IWrappedComponentProps, any> {
+  render() {
+    let passThroughProps = {};
+    let propClasses = [this.props.className];
+
+    Object.keys(this.props).forEach(key => {
+      if (!tachyons.has(key)) {
+        passThroughProps[key] = this.props[key];
+      } else if (Boolean(this.props[key])) {
+        propClasses.push(key);
+      }
+    });
+
+    return <WrappedComponent
+      {...passThroughProps}
+      className={propClasses.filter(isValidClassName).join(" ")}
+    />;
+  }
+}
+
+interface IWrappedComponentProps extends HTMLElement<any> {
+
+}
