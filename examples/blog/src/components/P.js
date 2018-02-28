d1fed806aa53ab88592deed982ba21c453ac6f35
Trey Hoover <treyhoover@gmail.com>
2018-02-27 22:16:53 -0500

    Revert "Add support for mapping class names"
    This reverts commit 525e203d1bd07aab085678be7007532fb0dc321c.

--- examples/blog/src/components/P.js
+++ examples/blog/src/components/P.js
@@ -1,17 +1,6 @@
 import * as React from "react";
-import { withCustomTachyons } from "reactyons"
-
-const P = withCustomTachyons({
-  map: {
-    "primary": "gold",
-    "bg-primary": "bg-gold",
-  },
-  overrides: {
-    "red": false,
-    "custom-class": true,
-  }
-})(p => <p {...p} />);
+import { P } from "reactyons"
 
 export default props => (
-  <P primary f5 f3-ns lh-copy measure georgia {...props} />
+  <P f5 f3-ns lh-copy measure georgia {...props} />
 );
